2a1c9d7c80c64f4cbfab6bff4800c7fdef898833
ShreyaM <shreyamalleshappa@gmail.com>
2026-04-08 23:31:57 +0530

    my project upload

---
+++ test.py
@@ -0,0 +1,10 @@
+import mediapipe as mp
+import sklearn
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import cv2
+import seaborn as sns
+
+print("✅ All libraries imported successfully!")
